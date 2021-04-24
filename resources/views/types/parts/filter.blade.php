9072bee54187fc46296cb640414d367e3681999d
carlos <drella01@icloud.com>
2021-04-24 19:38:42 +0200

    filter improves

--- resources/views/types/parts/filter.blade.php
+++ resources/views/types/parts/filter.blade.php
@@ -1,11 +1,12 @@
 <div class="py-2 text-white">
     <h3>Filtros</h3>
-    <h6 class="text-center text-white">Marcas</h6>
+    <p class="text-center text-white">Marca de vehículo</p>
     <ul class="mb-2" style="list-style: none">
         @foreach (App\Models\Brand::pluck('name') as $item)
-        <li>{{$item}}</li>
+        <li>{{ ucfirst($item) }}</li>
         @endforeach
     </ul>
+    <hr class="dropdown-divider">
     <p class="text-center text-white">Tipo de vehículo</p>
     <form action="" method="get" id="">
         <select class="select2 form-control" name="" id="" onchange="">
