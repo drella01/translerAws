4e866f3254257825791a02bc209aa7f243bf3743
carlos <drella01@icloud.com>
2021-05-12 14:04:18 +0200

    zoom gallery & migrate tank trailer

--- database/migrations/2021_04_29_144234_create_tank_trailers_table.php
+++ database/migrations/2021_04_29_144234_create_tank_trailers_table.php
@@ -15,6 +15,24 @@ class CreateTankTrailersTable extends Migration
     {
         Schema::create('tank_trailers', function (Blueprint $table) {
             $table->id();
+            $table->string('madeof')->nullable();
+            $table->string('fuel')->nullable();
+            $table->string('volume')->nullable();
+            $table->string('compartemnts')->nullable();
+            $table->string('liters1')->nullable();
+            $table->string('liters2')->nullable();
+            $table->string('liters3')->nullable();
+            $table->string('liters4')->nullable();
+            $table->string('liters5')->nullable();
+            $table->string('liters6')->nullable();
+            $table->string('degassed')->nullable();
+            $table->string('counter')->nullable();
+            $table->string('bombBrand')->nullable();
+            $table->string('minLPM')->nullable();
+            $table->string('maxLPM')->nullable();
+            $table->string('hose')->nullable();
+            $table->string('hose1Lenght')->nullable();
+            $table->string('hose2Lenght')->nullable();
             $table->timestamps();
         });
     }
---
+++ resources/views/vehicles/parts/zoomgallery.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Untitled</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link href="{{ asset('css/axZm.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/axZmCustom.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
+</head>
+
+<body>
+    <div class="photo-gallery">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">{{__('custom.gallery')}}</h2>
+                <p class="text-center">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae. </p>
+            </div>
+            <div class="row photos">
+                @foreach ($photos as $photo)
+                    <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ url($photo) }}" data-lightbox="photos"><img class="img-fluid" src="{{ url($photo) }}"></a></div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+    <script src="{{ asset('js/axZm.iframe.js') }}"></script>
+    <script src="{{ asset('js/jquery.axZm.js') }}"></script>
+    <script src="{{ asset('js/jquery.axZm.loader.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
+</body>
+
+</html>
