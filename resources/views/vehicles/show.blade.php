ed355233e5fdcc351448723f5e7f1b14dfa851bd
carlos <drella01@icloud.com>
2021-05-27 20:14:47 +0200

    modify show blade

--- resources/views/vehicles/show.blade.php
+++ resources/views/vehicles/show.blade.php
@@ -1,37 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="bg-custom text-white px-2 mb-0" style="margin-bottom:-2px;"><h2>You are here:</h2></div>
+<div class="bg-filter px-2" style="margin-top: -10px"><h2>Return to home page</h2></div>
 <div class="container">
     <div class="row">
         <div class="col-6">
-            <h1>{{ $vehicle->registration }}</h1>
+            <hr>
+            <div class="row">
+                <div class="col-6">
+                    <h2 class="px-2" style="font-family: Open Sans, sans-serif;font-size:26px; font-weight:bold;">{{ $vehicle->brand }}</h2>
+                </div>
+                <div class="col-6 text-right">
+                    <a href="{{ url('storage/pdf/'.$vehicle->registration.'.pdf') }}" class="btn btn-primary" target="_blank" rel="noopener noreferrer">View PDF</a>
+                </div>
+            </div>
+            <h3 class="px-2" style="font-family: Open Sans, sans-serif;font-size:20px; font-weight:bold;">{{ $vehicle->model }}</h3>
+            <hr>
         </div>
         <div class="col-4">
             <div class="intro">
-                <h2 class="text-center">{{__('custom.gallery')}}</h2>
+                <h2 class="text-center" style="font-family: Open Sans, sans-serif;font-size:26px; font-weight:bold;">{{__('custom.gallery')}}</h2>
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-6">
-            <div id="carouselPhotosControl" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                    @foreach ($photos as $photo)
-                    <div class="carousel-item">
-                        <img src="{{ url($photo) }}" class="d-block w-100" alt="...">
-                    </div>
-                    @endforeach
-                </div>
-                <a class="carousel-control-prev" href="#carouselPhotosControl" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselPhotosControl" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-            <div class="row my-4 text-center">
+            <div class="row text-center">
                 <div class="col-lg-12 bg-custom text-white">
                     <h5>{{ __('custom.general_data') }}</h5>
                 </div>
@@ -54,20 +49,37 @@
                     <td><h4>{{ $vehicle->sale_price.'€'}}</h4></td>
                 </tr>
             </table>
+            <div class="col-12">
+                <div class="row my-4 text-center">
+                    <div class="col-lg-12 bg-custom text-white">
+                        <h5>{{ __('custom.tank-trailer') }}</h5>
+                    </div>
+                </div>
+            </div>
+            @include('vehicles.parts.showTank')
         </div>
         <div class="col-4">
+            <div id="carouselPhotosControl" class="carousel slide mb-4" data-ride="carousel">
+                <div class="carousel-inner">
+                    @foreach ($photos as $photo)
+                    <div class="carousel-item">
+                        <img src="{{ url($photo) }}" class="d-block w-100" alt="...">
+                    </div>
+                    @endforeach
+                </div>
+                <a class="carousel-control-prev" href="#carouselPhotosControl" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselPhotosControl" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
             @include('vehicles.parts.epicgallery')
         </div>
     </div>
     <div class="row">
-        <div class="col-12">
-            <div class="row my-4 text-center">
-                <div class="col-lg-12 bg-custom text-white">
-                    <h5>{{ __('custom.tank-trailer') }}</h5>
-                </div>
-            </div>
-        </div>
-        @include('vehicles.parts.showTank')
         <a href="{{ url('storage/pdf/'.$vehicle->registration.'.pdf') }}" class="btn btn-primary" target="_blank" rel="noopener noreferrer">View PDF</a>
         <a href="{{ route('vehicles.edit',$vehicle) }}" class="btn btn-primary" target="_blank" rel="noopener noreferrer">Edit vehicle</a>
     </div>
